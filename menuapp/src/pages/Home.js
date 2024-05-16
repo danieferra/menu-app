c73f312db5e6b5c09e0a2ec10c3717ce4d46c5c6
danieferra <danieferra@gmail.com>
2024-05-16 18:47:44 +0100

    Home change

--- menuapp/src/pages/Home.js
+++ menuapp/src/pages/Home.js
@@ -8,13 +8,13 @@ function Home() {
         <div className="centro d-flex align-items-center flex-column justify-content-center p-2">
             <div className="card col-md-6 col-12 p-3">
                 <div className='row'>
-                <Link to={'/editarMenu'} className='col-6' style={{padding:'10px',textAlign:'center',textDecoration:'none',color:'#1e1e1e',fontWeight:'bold'}}>
+                <Link to={'/editarMenu'} className='col-12' style={{padding:'10px',textAlign:'center',textDecoration:'none',color:'#1e1e1e',fontWeight:'bold'}}>
                 <div style={{backgroundColor:'#efefef',padding:'20px',borderRadius:'12px'}}>Editar Menu</div>
                 </Link>
-                <Link to={'/pedir'} className='col-6' style={{padding:'10px',textAlign:'center',textDecoration:'none',color:'#1e1e1e',fontWeight:'bold'}}>
+                <Link to={'/pedir'} className='col-12' style={{padding:'10px',textAlign:'center',textDecoration:'none',color:'#1e1e1e',fontWeight:'bold'}}>
                 <div style={{backgroundColor:'#efefef',padding:'20px',borderRadius:'12px'}}>Registar pedidos</div>
                 </Link>
-                <Link to={'/verpedidos'} className='col-6' style={{padding:'10px',textAlign:'center',textDecoration:'none',color:'#1e1e1e',fontWeight:'bold'}}>
+                <Link to={'/verpedidos'} className='col-12' style={{padding:'10px',textAlign:'center',textDecoration:'none',color:'#1e1e1e',fontWeight:'bold'}}>
                 <div style={{backgroundColor:'#efefef',padding:'20px',borderRadius:'12px'}}>Ver pedidos</div>
                 </Link>
                 </div>
